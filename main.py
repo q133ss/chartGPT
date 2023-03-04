9dddcd03004e78944884cd8f55e549c72cfbc526
Alexey Miroshkin <blockstar2k@gmail.com>
2023-03-04 19:51:02 +0300

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import openai
+import keys
+
+openai.api_key = keys.api_key
+
+# задаем модель и промпт
+model_engine = "text-davinci-003"
+#Запрос
+prompt = "Как жарить пельмени?"
+
+# задаем макс кол-во слов
+max_tokens = 128
+
+# генерируем ответ
+completion = openai.Completion.create(
+    engine=model_engine,
+    prompt=prompt,
+    max_tokens=1024,
+    temperature=0.5,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+)
+
+# выводим ответ
+print(completion.choices[0].text) #ответ
